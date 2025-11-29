4905527ede8d8b08db4ae5cd18f7466621a59345
Patrick Oliveira Vagetti <77699722+DevFalconsz@users.noreply.github.com>
2025-11-29 11:35:58 -0300

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Configurações de ambiente para produção.
- * Estas configurações são usadas quando a aplicação é compilada para produção.
- */
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8080'
-};
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Configurações de ambiente para desenvolvimento.
- * Estas configurações são usadas quando a aplicação roda em modo de desenvolvimento.
- */
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080'
-};
